1832831f5ae7ff534c8283a1c12de7811b716b04
ZiF1R <do-alex03@mail.ru>
2022-04-03 20:56:35 +0300

    Fix primary colors and form input states

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,29 @@ for (let smoothLink of smoothLinks) {
         });
     });
 };
+
+let primaryInputs = document.querySelectorAll(".auth-form .input_primary");
+
+for (let input of primaryInputs) {
+  input.classList.add("input_start");
+
+  input.addEventListener("input", (e) => {
+    let val = e.target.value,
+      target = e.target;
+
+    if (val !== "") {
+      target.classList.add("input_active");
+    } else {
+      target.classList.remove("input_active");
+    }
+  });
+
+  input.addEventListener("blur", (e) => {
+    let target = e.target;
+    if (target.value !== "") {
+      target.classList.remove("input_start");
+    } else {
+      input.classList.add("input_start");
+    }
+  });
+}
